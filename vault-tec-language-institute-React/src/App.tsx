1bd48b177ab6fac5098cc53e86f94b6aa099acfb
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-11 21:45:58 +0300

    [edit] main + app

--- vault-tec-language-institute-React/src/App.tsx
+++ vault-tec-language-institute-React/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import About from './Pages/About/About.tsx'
 import Chat from './Pages/Chat/Chat.tsx'
 import Check from './Pages/Check/Check.tsx'
@@ -12,23 +12,21 @@ import Speech from './Pages/Speech/Speech.tsx'
 
 export default function App() {
 	return (
-		<Router>
-			<div className='app-container'>
-				<div className='content'>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/login' element={<Login />} />
-						<Route path='/signup' element={<Signup />} />
-						<Route path='/about' element={<About />} />
-						<Route path='/dashboard' element={<Dashboard />} />
-						<Route path='/chat' element={<Chat />} />
-						<Route path='/speech' element={<Speech />} />
-						<Route path='/check' element={<Check />} />
-						<Route path='/exercises' element={<Exercises />} />
-						<Route path='/progress' element={<Progress />} />
-					</Routes>
-				</div>
+		<div className='app-container'>
+			<div className='content'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<Signup />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/chat' element={<Chat />} />
+					<Route path='/speech' element={<Speech />} />
+					<Route path='/check' element={<Check />} />
+					<Route path='/exercises' element={<Exercises />} />
+					<Route path='/progress' element={<Progress />} />
+				</Routes>
 			</div>
-		</Router>
+		</div>
 	)
 }
