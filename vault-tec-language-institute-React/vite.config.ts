76029a9a7c433404b14fd34efb93da5526712594
Trykach34rus <smoke14071995Andres@gmail.com>
2025-09-11 21:36:42 +0300

    [edit] vite.config

--- vault-tec-language-institute-React/vite.config.ts
+++ vault-tec-language-institute-React/vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'vault-tec-lenguage-institute-React ',
+	base: 'vault-tec-lenguage-institute-React',
 })
